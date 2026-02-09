538197cc172de5cd5412ec8d93d293fe82190a1f
daeservidorvg-art <daeservidorvg@gmail.com>
2026-02-09 18:54:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const sw = globalThis;
-
-const CACHE_NAME = 'v1_cache_pwa';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-sw.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('dae-v1').then((cache) => {
+      return cache.addAll(['index.html', 'manifest.json']);
     })
   );
 });
 
-sw.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
